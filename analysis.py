fb1bc4eae0348a48af54cf5ade471c19d6262643
Bucchiman <iwabuchi@fun.bio.keio.ac.jp>
2022-05-08 16:07:09 +0900

    analysis: facet distribution

--- analysis.py
+++ analysis.py
@@ -5,6 +5,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+'''
+    数値オブジェクトのコラムに関して
+    facetにより出力する
+'''
+def distribution_facet(df, columns):
+    f = pd.melt(df, value_vars=columns)
+    g = sns.FacetGrid(f, col="variable",  col_wrap=2, ax=ax, sharex=False, sharey=False)
+    g = g.map(sns.distplot, "value")
+    plt.savefig("distribution_facet.pdf")
+
+
+
+
 def main():
     pass
 
