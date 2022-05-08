eb7dc13efab7051ad636f29260ecdf8543fe9388
Bucchiman <iwabuchi@fun.bio.keio.ac.jp>
2022-05-08 15:47:12 +0900

    initial commit

---
+++ preprocess.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+
+class Preprocess(object):
+    def __init__(self):
+        '''
+        '''
+        pass
+
+    def fill_na(self):
+        '''
+        '''
+        pass
+
+    def drop_features(self):
+        '''
+        '''
+        pass
+
+
+    def create_features(self):
+        '''
+        '''
+        pass
+
+    def convert_features(self):
+        '''
+        '''
+        pass
